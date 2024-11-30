ae7694e5cbe07e1717cbda36f7c7d85d5e7c8834
Prakhar Patil <prakhar_patil@Prakhars-MacBook-Air.local>
2024-11-30 09:12:00 +0530

    first commit

---
+++ StudentESD/src/main/java/com/prakharpatil/studentesd/StudentEsdApplication.java
@@ -0,0 +1,13 @@
+package com.prakharpatil.studentesd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentEsdApplication.class, args);
+    }
+
+}
---
+++ StudentESD/src/test/java/com/prakharpatil/studentesd/StudentEsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakharpatil.studentesd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
